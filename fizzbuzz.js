4eeebc4c09b66a0d93e722daa628247adab52651
chipembere <chitiyobrian@gmail.com>
2019-11-07 15:37:38 +0000

    Complete

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,18 +1,13 @@
 function fizzbuzz(num) {
-    
     if (num % 3 === 0 && num % 5 === 0) {
-        return fizzbuzz
-    }
-
-    else if (num % 3 === 0) {
-        return "Fizz";
-    }
-    else (num % 5 === 0) {
-        return "Buzz"
-    }
-
+        return 'fizzbuzz'
+    } else if (num % 3 === 0) {
+        return 'Fizz'
+    } else if (num % 5 === 0) {
+        return 'Buzz'
+    } else { return 'Even number' }
 }
 
-for(i=0; i<100;i++) {
+for (i = 0; i < 100; i++) {
     console.log(fizzbuzz(i))
 }
