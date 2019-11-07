597da71cf0f3ff81d2948ae7b273f65a4de8d145
Darnell23 <darnell.thomas@hotmail.co.uk>
2019-11-07 15:24:20 +0000

    added objects

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,6 +1,18 @@
 function fizzbuzz(num) {
-    if (num % 3 === 0) {
-        return "Fizz";
+    
+    if (num % 3 === 0 && num % 5 === 0) {
+        return fizzbuzz
     }
 
+    else if (num % 3 === 0) {
+        return "Fizz";
+    }
+    else (num % 5 === 0) {
+        return "Buzz"
+    }
+
+}
+
+for(i=0; i<100;i++) {
+    console.log(fizzbuzz(i))
 }
